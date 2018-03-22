792d1d423f591964f3236618b14eb4253a4ea0ad
Andres Pardini <unpardos@gmail.com>
2018-03-22 16:34:02 -0300

    gh pages v2

--- config/index.js
+++ config/index.js
@@ -50,7 +50,7 @@ module.exports = {
     // Paths
     assetsRoot: path.resolve(__dirname, '../dist'),
     assetsSubDirectory: 'static',
-    assetsPublicPath: '/',
+    assetsPublicPath: '/pyday/',
 
     /**
      * Source Maps
