1ea2bb2f3c08201a4f597458d5e3b1d082fb3dbf
Diego Duncan <diegoduncan21@gmail.com>
2018-03-30 01:29:03 -0300

    probando deploy.

--- config/index.js
+++ config/index.js
@@ -50,7 +50,7 @@ module.exports = {
     // Paths
     assetsRoot: path.resolve(__dirname, '../docs'),
     assetsSubDirectory: 'static',
-    assetsPublicPath: '/pydaynea/',
+    assetsPublicPath: '/',
 
     /**
      * Source Maps
